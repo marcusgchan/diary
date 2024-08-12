d990d2c1dfa2880f22d3fc3ebd349c5ae8d1a7aa
Marcus Chan <marcuscchan@gmail.com>
2024-08-12 12:53:45 -0700

    remove homepage

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/diaries",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
