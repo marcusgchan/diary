d2c340b42315b14dce0109ff03a19443f2809922
Marcus Chan <marcuscchan@gmail.com>
2024-08-15 14:13:31 -0700

    update endpoint

--- src/server/s3Client.ts
+++ src/server/s3Client.ts
@@ -2,7 +2,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 import { env } from "~/env.mjs";
 
 export const s3Client = new S3Client({
-  endpoint: env.BUCKET_URL,
+  endpoint: env.NODE_ENV !== "production" ? env.BUCKET_URL : undefined,
   forcePathStyle: env.NODE_ENV !== "production",
   region: "us-west-1",
   credentials: {
