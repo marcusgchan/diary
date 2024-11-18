66dc1b0a530a4eb2b912d8510865a1e17355aea7
Marcus Chan <marcuscchan@gmail.com>
2024-11-17 21:11:16 -0800

    chore: rm migration for now

--- src/server/db/migrations/0001_silent_shockwave.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "diary" ADD COLUMN "deleting" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "entry" ADD COLUMN "deleting" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "image_key" ADD COLUMN "deleting" boolean DEFAULT false NOT NULL;
