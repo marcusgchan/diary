b76b2f0f91a4496f27f5f477de6bd67009e58aa6
Marcus Chan <marcuscchan@gmail.com>
2024-12-04 16:50:12 -0800

    refactor: db credentials

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./src/server/db/schema.ts",
   dbCredentials: {
-    // url: `postgres://${env.DATABASE_USER}:${env.DATABASE_PASS}@${env.DATABASE_HOST}:${env.DATABASE_PORT}/${env.DATABASE_NAME}`,
-    url: `postgres://${env.DATABASE_USER}:${env.DATABASE_PASS}@localhost:${env.DATABASE_PORT}/${env.DATABASE_NAME}`,
+    url: `postgres://${env.DATABASE_USER}:${env.DATABASE_PASS}@${env.DATABASE_HOST}:${env.DATABASE_PORT}/${env.DATABASE_NAME}`,
   },
   verbose: true,
+  schemaFilter: ["public"],
 });
