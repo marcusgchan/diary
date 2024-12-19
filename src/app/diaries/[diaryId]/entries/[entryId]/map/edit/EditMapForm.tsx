dc72ebf4abfb419a0a4cf483bbf45011bddc8be5
Marcus Chan <marcuscchan@gmail.com>
2024-12-19 13:33:23 -0800

    form errors

--- src/app/diaries/[diaryId]/entries/[entryId]/map/edit/EditMapForm.tsx
+++ src/app/diaries/[diaryId]/entries/[entryId]/map/edit/EditMapForm.tsx
@@ -4,19 +4,26 @@ import { Button } from "~/app/_components/ui/button";
 import { Input } from "~/app/_components/ui/input";
 import { Label } from "~/app/_components/ui/label";
 import { Textarea } from "~/app/_components/ui/textarea";
-import { SubmitHandler, useFieldArray, useForm } from "react-hook-form";
+import {
+  SubmitHandler,
+  useFieldArray,
+  useForm,
+  FormProvider,
+  useFormContext,
+} from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ChangeEvent, ChangeEventHandler } from "react";
+import { ChangeEvent } from "react";
+import { cn } from "~/app/_utils/cx";
 
 const formSchema = z.object({
   posts: z
     .object({
       title: z.string(),
       image: z.object({
-        name: z.string().min(1),
-        size: z.number().min(1),
-        type: z.string().min(1),
+        name: z.string(),
+        size: z.number().max(8920, { message: "Max image size is 8.9MB" }),
+        mimetype: z.string().min(1, { message: "Image Required" }),
       }),
       description: z.string(),
     })
@@ -25,13 +32,7 @@ const formSchema = z.object({
 
 type FormValues = z.infer<typeof formSchema>;
 export function EditMapForm() {
-  const {
-    control,
-    register,
-    setValue,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>({
+  const formMethods = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       posts: [
@@ -40,13 +41,21 @@ export function EditMapForm() {
           image: {
             name: "",
             size: 0,
-            type: "",
+            mimetype: "",
           },
           description: "",
         },
       ],
     },
   });
+  const {
+    control,
+    register,
+    setValue,
+    resetField,
+    handleSubmit,
+    formState: { errors },
+  } = formMethods;
   const { fields, append, remove, swap } = useFieldArray({
     control,
     name: "posts",
@@ -57,87 +66,132 @@ export function EditMapForm() {
       title: "",
       description: "",
       image: {
-        type: "",
+        mimetype: "",
         size: 0,
         name: "",
       },
     });
   }
-  register(`posts.0.image`);
+  function removePost(index: number) {
+    remove(index);
+  }
 
-  function handleImageUpload(payload: ImagePayload) {
-    setValue("posts.0.image", payload);
+  function handleImageUpload(index: number, payload: ImagePayload | null) {
+    console.log({ payload });
+    if (payload) {
+      setValue(`posts.${index}.image`, payload, { shouldValidate: true });
+      return;
+    }
+    resetField(`posts.${index}`);
   }
 
   const onSubmit: SubmitHandler<FormValues> = (e) => {};
 
   return (
-    <form className="grid max-w-md gap-4" onSubmit={handleSubmit(onSubmit)}>
-      {fields.map((field, index) => {
-        const fieldErrors = errors?.posts?.[index];
-        console.log(fieldErrors);
-        return (
-          <fieldset key={field.id} className="grid gap-2">
-            <div>
-              <Label htmlFor="title">Title</Label>
-              <Input
-                {...register(`posts.${index}.title`)}
-                id="title"
-                type="text"
-                placeholder="Whistler at Night"
-              />
-            </div>
-            <ImageUpload onChange={handleImageUpload} />
-            <div>
-              <Label htmlFor="description">Description</Label>
-              <Textarea
-                {...register(`posts.${index}.description`)}
-                id="description"
-              />
-            </div>
-          </fieldset>
-        );
-      })}
-      <button
-        onClick={addPost}
-        type="button"
-        className="rounded border-2 border-muted"
-      >
-        +
-      </button>
-      <Button>Save</Button>
-    </form>
+    <FormProvider {...formMethods}>
+      <form className="grid max-w-md gap-4" onSubmit={handleSubmit(onSubmit)}>
+        {fields.map((field, index) => {
+          const fieldErrors = errors?.posts?.[index];
+          console.log(fieldErrors);
+          return (
+            <fieldset
+              key={field.id}
+              className="relative grid gap-2 bg-gray-100"
+            >
+              <button
+                onClick={() => remove(index)}
+                className="absolute right-0 top-0 -translate-y-1/2 translate-x-1/2"
+              >
+                X
+              </button>
+              <div>
+                <Label htmlFor="title">Title</Label>
+                <Input
+                  {...register(`posts.${index}.title`)}
+                  id="title"
+                  type="text"
+                  placeholder="Whistler at Night"
+                />
+              </div>
+              <ImageUpload onChange={handleImageUpload} index={index} />
+              <div>
+                <Label htmlFor="description">Description</Label>
+                <Textarea
+                  {...register(`posts.${index}.description`)}
+                  id="description"
+                />
+              </div>
+            </fieldset>
+          );
+        })}
+        <button
+          onClick={addPost}
+          type="button"
+          className="rounded border-2 border-muted"
+        >
+          +
+        </button>
+        <Button>Save</Button>
+      </form>
+    </FormProvider>
   );
 }
 
 type ImagePayload = {
   name: string;
   size: number;
-  type: string;
+  mimetype: string;
 };
 
-function ImageUpload({ onChange }: { onChange: (p: ImagePayload) => void }) {
+function ImageUpload({
+  onChange,
+  index,
+}: {
+  onChange: (index: number, p: ImagePayload | null) => void;
+  index: number;
+}) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<FormValues>();
   function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
     const files = e.target.files;
     if (files === null) {
+      onChange(index, null);
       return;
     }
 
     const file = files[0];
     if (file === undefined) {
+      onChange(index, null);
       return;
     }
 
-    onChange({
+    onChange(index, {
       name: file.name,
       size: file.size,
-      type: file.type,
+      mimetype: file.type,
     });
   }
+  register(`posts.${index}.image`);
+  const error = errors.posts?.[index]?.image;
+  const hasError = !!error?.size?.message || !!error?.mimetype?.message;
+  console.log(!!error?.size?.message, !!error?.mimetype?.message);
   return (
     <div className="grid w-full items-center gap-1.5">
       <Label htmlFor="image">Image</Label>
-      <Input type="file" accept="image/*" onChange={handleFileChange} />
+      <Input
+        type="file"
+        accept="image/*"
+        className={cn(hasError && "border-red-300")}
+        onChange={handleFileChange}
+      />
+      {hasError && (
+        <div className="bg-red-300 p-2">
+          {error.size?.message && <p>{error.size.message}</p>}
+          {error.mimetype?.message && <p>{error.mimetype.message}</p>}
+        </div>
+      )}
     </div>
   );
 }
