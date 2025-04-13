1a8e2f3b92c25284f78e07a8f6635902a44de75a
Marcus Chan <marcuscchan@gmail.com>
2025-04-13 15:16:18 -0400

    try dis

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,15 @@
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next", "next/typescript", "prettier"],
+    extends: ["next/core-web-vitals", "next", "prettier"],
   }),
   ...tseslint.configs.recommendedTypeChecked,
   {
@@ -38,3 +40,21 @@ const eslintConfig = [
 ];
 
 export default eslintConfig;
+//
+// import js from "@eslint/js";
+// import { FlatCompat } from "@eslint/eslintrc";
+//
+//
+// const compat = new FlatCompat({
+//   // import.meta.dirname is available after Node.js v20.11.0
+//   baseDirectory: import.meta.dirname,
+//   recommendedConfig: js.configs.recommended,
+// });
+//
+// const eslintConfig = [
+//   ...compat.config({
+//     extends: ["eslint:recommended", "next"],
+//   }),
+// ];
+//
+// export default eslintConfig;
